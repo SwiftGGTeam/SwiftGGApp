6770625482490dc99908232e4fd8349fc86be974
TangJR <tangjr.work@gmail.com>
2015-12-01 00:02:54 +0800

    Login设为Root

--- SwiftGG/AppDelegate.swift
+++ SwiftGG/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let loginVC = SGLoginViewController()
+        self.window!.rootViewController = loginVC
+        
         return true
     }
 
