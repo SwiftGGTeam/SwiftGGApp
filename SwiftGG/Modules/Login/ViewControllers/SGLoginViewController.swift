103dd5d45def52696165fc6920f5388d5ce62185
TangJR <tangjr.work@gmail.com>
2015-12-01 00:46:41 +0800

    登录界面基本完成

--- SwiftGG/Modules/Login/ViewControllers/SGLoginViewController.swift
+++ SwiftGG/Modules/Login/ViewControllers/SGLoginViewController.swift
@@ -15,21 +15,12 @@ class SGLoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(animated: Bool) {
+        UIApplication.sharedApplication().statusBarStyle = .Default
     }
-    */
-
 }
