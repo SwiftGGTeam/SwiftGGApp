062afef29c31622818c2f54da3b5c9e2db364d81
TangJR <tangjr.work@gmail.com>
2015-11-30 23:56:17 +0800

    创建LoginViewController

---
+++ SwiftGG/Modules/Login/ViewControllers/SGLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SGLoginViewController.swift
+//  SwiftGG
+//
+//  Created by TangJR on 11/30/15.
+//  Copyright © 2015 swiftgg. All rights reserved.
+//
+
+import UIKit
+
+class SGLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
