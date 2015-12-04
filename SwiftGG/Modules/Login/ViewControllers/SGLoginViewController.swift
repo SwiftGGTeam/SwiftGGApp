b2d6f78b6aff4a549c628ccd48ded8328f08bea2
WordBeyond <wordbeyondyoung@gmail.com>
2015-12-04 15:23:41 +0800

    手动创建主界面Window

--- SwiftGG/Modules/Login/ViewControllers/SGLoginViewController.swift
+++ SwiftGG/Modules/Login/ViewControllers/SGLoginViewController.swift
@@ -12,15 +12,9 @@ class SGLoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-    override func viewWillAppear(animated: Bool) {
-        UIApplication.sharedApplication().statusBarStyle = .LightContent
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        UIApplication.sharedApplication().statusBarStyle = .Default
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
 }
