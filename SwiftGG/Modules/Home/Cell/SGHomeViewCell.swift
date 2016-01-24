5f6afdec84025facc62658f445d4f8a68c5fc91e
AlanMelody <alanmelody@qq.com>
2016-01-24 20:59:54 +0800

    添加HomeViewCell

---
+++ SwiftGG/Modules/Home/Cell/SGHomeViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SGHomeViewCell.swift
+//  SwiftGG
+//
+//  Created by Jack on 16/1/24.
+//  Copyright © 2016年 swiftgg. All rights reserved.
+//
+
+import UIKit
+
+class SGHomeViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
