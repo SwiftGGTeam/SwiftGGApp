9ae0a9b1c14bab8cabc41ac27a24d82571681a2b
AlanMelody <alanmelody@qq.com>
2016-01-24 23:34:07 +0800

    主页Cell

--- SwiftGG/Modules/Home/Cell/SGHomeViewCell.swift
+++ SwiftGG/Modules/Home/Cell/SGHomeViewCell.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class SGHomeViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var assistView: UIView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +27,17 @@ class SGHomeViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func configureCell(cellModel: HomeViewCellModel) {
+        if let avatarUrl = NSURL(string: cellModel.avatarUrl) {
+            avatarImageView.kf_setImageWithURL(avatarUrl, placeholderImage: UIImage(named: "about_logo"))
+        }
+        titleLabel.text = cellModel.title
+        descriptionLabel.text = cellModel.description
+        // Todo
+        authorLabel.text = cellModel.author
+        dateLabel.text = cellModel.date
+    }
+    
     
 }
--- SwiftGG/Modules/Home/ViewControllers/SGHomeViewController.swift
+++ SwiftGG/Modules/Home/ViewControllers/SGHomeViewController.swift
@@ -1,18 +1,38 @@
 //
-//  SGLoginViewController.swift
+//  SGHomeViewController.swift
 //  SwiftGG
 //
-//  Created by TangJR on 11/30/15.
-//  Copyright © 2015 swiftgg. All rights reserved.
+//  Created by JackAlan on 01/22/16.
+//  Copyright © 2016 swiftgg. All rights reserved.
 //
 
 import UIKit
 
-class SGHomeViewController: UIViewController {
+let cellId = "HomeViewCell"
+
+class SGHomeViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         title = "首页"
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44.0
+        tableView.registerNib(UINib(nibName: "SGHomeViewCell", bundle: nil), forCellReuseIdentifier: cellId)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId,forIndexPath: indexPath) as! SGHomeViewCell
+        return cell;
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 10;
     }
 }
