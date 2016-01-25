54b6b3d61915a7f124636a9ce6e0f287763e4923
mmoaay <perry@perrydemac-mini.local>
2016-01-25 18:44:05 +0800

    1.uiwebview替换成wkwebview 2.返回按钮支持网页内导航

--- SwiftGG/Modules/ArticleDetail/ViewContoller/SGArticleDetailViewController.swift
+++ SwiftGG/Modules/ArticleDetail/ViewContoller/SGArticleDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Neon
 import WebKit
 
 protocol SGArticleDetailInfoProtocol: class {
@@ -17,7 +18,7 @@ class SGArticleDetailViewController: UIViewController {
     
     weak var articleDetailInfoProtocol : SGArticleDetailInfoProtocol?
     
-    @IBOutlet weak var articleContentView: UIWebView!
+    var articleContentView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,14 +30,27 @@ class SGArticleDetailViewController: UIViewController {
     
     // MARK: Helper Methods
     private func initArticleContentView() {
+        articleContentView = WKWebView()
+        view.insertSubview(articleContentView, atIndex: 0)
+        articleContentView.allowsBackForwardNavigationGestures = true
+        
         if let articleInfo = articleDetailInfoProtocol?.getSGArticleDetailInfo() {
             let url = NSURL(string: articleInfo.url)
-            //            let request = NSURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: NSTimeInterval(30))
+//            let request = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: NSTimeInterval(30))
             let request = NSURLRequest(URL: url!)
             articleContentView.loadRequest(request)
             
             title = articleInfo.title
         }
+        
+        
+    }
+    
+    // WARN:neon框架需要在autolayout渲染完之后再使用
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        articleContentView.fillSuperview()
     }
     
     private func setupNavigationBar() {
@@ -44,7 +58,12 @@ class SGArticleDetailViewController: UIViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: backImage, style: .Plain, target: self, action: Selector("back"))
     }
     
+    
     func back() {
-        navigationController!.popViewControllerAnimated(true)
+        if articleContentView.canGoBack {
+            articleContentView.goBack()
+        } else {
+            navigationController!.popViewControllerAnimated(true)
+        }
     }
 }
