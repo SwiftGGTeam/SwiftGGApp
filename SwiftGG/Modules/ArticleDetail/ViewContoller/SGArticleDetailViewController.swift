8a6dede34816c4cc73566e454f6ad182986c085d
WordBeyond <wordbeyondyoung@gmail.com>
2016-01-25 12:54:04 +0800

    修改文章详情返回按钮

--- SwiftGG/Modules/ArticleDetail/ViewContoller/SGArticleDetailViewController.swift
+++ SwiftGG/Modules/ArticleDetail/ViewContoller/SGArticleDetailViewController.swift
@@ -18,20 +18,33 @@ class SGArticleDetailViewController: UIViewController {
     weak var articleDetailInfoProtocol : SGArticleDetailInfoProtocol?
     
     @IBOutlet weak var articleContentView: UIWebView!
-    
-    private func initArticleContentView() {
-
-        if let articleInfo = articleDetailInfoProtocol?.getSGArticleDetailInfo() {
-            let url = NSURL(string: articleInfo.url)
-            //            let request = NSURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: NSTimeInterval(30))
-            let request = NSURLRequest(URL: url!)
-            articleContentView.loadRequest(request)
-        }
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initArticleContentView()
+        
+        setupNavigationBar()
+    }
+    
+    // MARK: Helper Methods
+    private func initArticleContentView() {
+        if let articleInfo = articleDetailInfoProtocol?.getSGArticleDetailInfo() {
+            let url = NSURL(string: articleInfo.url)
+            //            let request = NSURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: NSTimeInterval(30))
+            let request = NSURLRequest(URL: url!)
+            articleContentView.loadRequest(request)
+            
+            title = articleInfo.title
+        }
+    }
+    
+    private func setupNavigationBar() {
+        let backImage = UIImage(named: "back_white")?.imageWithRenderingMode(.AlwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: backImage, style: .Plain, target: self, action: Selector("back"))
+    }
+    
+    func back() {
+        navigationController!.popViewControllerAnimated(true)
     }
 }
