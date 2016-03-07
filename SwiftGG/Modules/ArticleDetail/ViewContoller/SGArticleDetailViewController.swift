01642f196a3afe40ab02f940bad15512ad3585eb
mmoaay <perry@perrydemac-mini.local>
2016-03-07 11:27:44 +0800

    1.添加滑动时隐藏显示导航栏功能 2.修改打开跳转链接为SFSafariViewController打开

--- SwiftGG/Modules/ArticleDetail/ViewContoller/SGArticleDetailViewController.swift
+++ SwiftGG/Modules/ArticleDetail/ViewContoller/SGArticleDetailViewController.swift
@@ -11,6 +11,7 @@ import Neon
 import WebKit
 import RxSwift
 import RxCocoa
+import SafariServices
 
 protocol SGArticleDetailInfoProtocol: class {
     func openArticle() -> SGArticleDetailInfo
@@ -27,15 +28,69 @@ public extension WKWebView {
     }
 }
 
-extension SGArticleDetailViewController {
-    func webView(webView: WKWebView, didCommitNavigation navigation: WKNavigation!) {
+extension SGArticleDetailViewController:UIScrollViewDelegate {
+    private func setupScrollView() {
+        self.articleContentView.scrollView.delegate = self
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        lastContentOffset = scrollView.contentOffset.y
+    }
+
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.contentOffset.y > lastContentOffset && scrollView.contentOffset.y >= 40.0 {
+            self.navigationController?.setNavigationBarHidden(true, animated: true)
+        } else {
+            self.navigationController?.setNavigationBarHidden(false, animated: true)
+        }
+    }
+}
+
+extension SGArticleDetailViewController:SFSafariViewControllerDelegate,WKNavigationDelegate {
+    
+    func webView(webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         self.recordOffset()
+        if webView.URL?.absoluteString != self.articleDetailInfo?.url {
+            if #available(iOS 9.0, *) {
+                let svc = SFSafariViewController(URL:webView.URL!, entersReaderIfAvailable:true)
+                svc.delegate = self
+                self.presentViewController(svc, animated:true, completion: nil)
+            } else {
+                // Fallback on earlier versions
+                UIApplication.sharedApplication().openURL(webView.URL!)
+            }
+        }
+    }
+    
+    @available(iOS 9.0, *)
+    func safariViewControllerDidFinish(controller: SFSafariViewController)
+    {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationResponse navigationResponse: WKNavigationResponse, decisionHandler: (WKNavigationResponsePolicy) -> Void) {
+        if webView.URL?.absoluteString == self.articleDetailInfo?.url {
+            decisionHandler(WKNavigationResponsePolicy.Allow)
+        } else {
+            decisionHandler(WKNavigationResponsePolicy.Cancel)
+        }
     }
     
     func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
         if webView.URL?.absoluteString == self.articleDetailInfo?.url {
-            let js = "$('body').scrollTop(" + (self.articleDetailInfo?.getOffset())! + ")"
+            let js = "$('body').scrollTop(" + self.articleDetailInfo!.getOffset() + ")"
             articleContentView.evaluateJavaScript(js, completionHandler: nil)
+        }
+    }
+    
+    func backPressed() {
+        if articleContentView.canGoBack {
+            articleContentView.goBack()
+        }
+    }
+    func forwardPressed() {
+        if articleContentView.canGoForward {
+            articleContentView.goForward()
         }
     }
     
@@ -50,45 +105,9 @@ extension SGArticleDetailViewController {
             })
         }
     }
-}
-
-extension SGArticleDetailViewController {
-    func backPressed() {
-        if articleContentView.canGoBack {
-            articleContentView.goBack()
-        }
-    }
-    func forwardPressed() {
-        if articleContentView.canGoForward {
-            articleContentView.goForward()
-        }
-    }
-}
-
-class SGArticleDetailViewController: UIViewController, WKNavigationDelegate, SGArticleDetailToolBarProtocol {
-    
-    weak var articleDetailInfoProtocol : SGArticleDetailInfoProtocol?
-    
-    var articleDetailInfo: SGArticleDetailInfo?
-    
-    var articleContentView: WKWebView!
-    
-    var articleDetailToolBar: SGArticleDetailToolBar?
-    
-    var disposeBag = DisposeBag()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        initArticleContentView()
-        
-        setupNavigationBar()
-        
-        setupArticleDetailToolBar()
-    }
     
     // MARK: Helper Methods
-    private func initArticleContentView() {
+    private func setupArticleContentView() {
         // 设置webView
         articleContentView = WKWebView()
         view.insertSubview(articleContentView, atIndex: 0)
@@ -101,24 +120,54 @@ class SGArticleDetailViewController: UIViewController, WKNavigationDelegate, SGA
             title = articleDetail.title
             
             let url = NSURL(string: articleDetail.url)
-//            let request = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: NSTimeInterval(30))
+            //            let request = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: NSTimeInterval(30))
             let request = NSURLRequest(URL: url!)
             articleContentView.loadRequest(request)
         }
     }
-    
+
+}
+
+extension SGArticleDetailViewController: SGArticleDetailToolBarProtocol {
     private func setupArticleDetailToolBar () {
         articleDetailToolBar = SGArticleDetailToolBar()
         articleDetailToolBar?.delegate = self
-        view.addSubview((articleDetailToolBar?.backButton)!)
-        view.addSubview((articleDetailToolBar?.forwardButton)!)
+        view.addSubview(articleDetailToolBar!.backButton)
+        view.addSubview(articleDetailToolBar!.forwardButton)
         
-//        articleContentView.rx_canGoBack
-//            .bindTo((articleDetailToolBar?.backButton)!.rx_enabled)
-//            .addDisposableTo(disposeBag)
-//        articleContentView.rx_canGoForward
-//            .bindTo((articleDetailToolBar?.forwardButton)!.rx_enabled)
-//            .addDisposableTo(disposeBag)
+        articleContentView.rx_canGoBack
+            .bindTo(articleDetailToolBar!.backButton.rx_enabled)
+            .addDisposableTo(disposeBag)
+        articleContentView.rx_canGoForward
+            .bindTo(articleDetailToolBar!.forwardButton.rx_enabled)
+            .addDisposableTo(disposeBag)
+    }
+}
+
+class SGArticleDetailViewController: UIViewController {
+    
+    weak var articleDetailInfoProtocol : SGArticleDetailInfoProtocol?
+    
+    var articleDetailInfo: SGArticleDetailInfo?
+    
+    var articleContentView: WKWebView!
+    
+    var articleDetailToolBar: SGArticleDetailToolBar?
+    
+    var disposeBag = DisposeBag()
+    
+    var lastContentOffset:CGFloat = 0.0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupArticleContentView()
+        
+        setupNavigationBar()
+        
+        setupScrollView()
+        
+//        setupArticleDetailToolBar()
     }
     
     // WARN:neon框架需要在autolayout渲染完之后再使用
