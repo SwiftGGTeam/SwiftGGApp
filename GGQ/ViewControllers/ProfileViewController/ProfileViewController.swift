bae46099d861990558b8e35741d2539588c137a4
DianQK <xiaoqing@dianqk.org>
2016-05-03 00:04:24 +0800

    Add Profile

--- GGQ/ViewControllers/ProfileViewController/ProfileViewController.swift
+++ GGQ/ViewControllers/ProfileViewController/ProfileViewController.swift
@@ -12,8 +12,35 @@ import RxCocoa
 import RxOptional
 import NSObject_Rx
 import SwiftyJSON
+import Kingfisher
 
 final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    var viewModel: ProfileViewModel!
+    
+    override func viewDidLoad() {
+        
+        viewModel = ProfileViewModel()
+        
+        viewModel.avatarURL
+            .subscribeNext { [unowned self] in
+                self.avatarImageView.kf_setImageWithURL($0)
+            }
+            .addDisposableTo(rx_disposeBag)
+        
+        viewModel.userName
+            .bindTo(userNameLabel.rx_text)
+            .addDisposableTo(rx_disposeBag)
+        
+        viewModel.logined.filter { !$0 }
+            .map { _ in "未登录" }
+            .bindTo(userNameLabel.rx_text)
+            .addDisposableTo(rx_disposeBag)
+    }
 
 	override func viewWillAppear(animated: Bool) {
 		navigationController?.setNavigationBarHidden(true, animated: true)
@@ -26,6 +53,10 @@ extension ProfileViewController: Routerable {
     }
     
     func post(url: NSURL, sender: JSON?) {
+        log.info("\(sender)")
+        if let token = sender?["token"].string {
+            viewModel.save(.GitHub, token: token)
+        }
         dismissViewControllerAnimated(true, completion: nil)
     }
 }
--- GGQ/ViewControllers/ProfileViewController/ProfileViewModel.swift
+++ GGQ/ViewControllers/ProfileViewController/ProfileViewModel.swift
@@ -6,4 +6,60 @@
 //  Copyright © 2016年 org.dianqk. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import RealmSwift
+
+class ProfileViewModel {
+    
+    let avatarURL: Observable<NSURL>
+    
+    let userName: Observable<String>
+    
+    let logined: Observable<Bool>
+    
+    private let tokenSubject = PublishSubject<Token>()
+    
+    private let disposeBag = DisposeBag()
+    
+    init() {
+        let realm = try! Realm()
+        
+        let optionalUser = realm.objects(UserModel).asObservable()
+            .map { $0.first }
+            .shareReplay(1)
+        
+        let updateToken = tokenSubject.asObservable()
+        
+        let noUser = optionalUser.map { $0 == nil }
+        
+        [updateToken, noUser.filter { $0 }].toObservable()
+            .map { _ in TokenType.GitHub }
+            .filter(KeychainService.exist)
+            .map { _ in }
+            .flatMapLatest { GGProvider.request(GitHubAPI.User).mapJSON() }
+            .flatMapLatest { Realm.rx_create(UserModel.self, value: $0.object, update: true) }
+            .subscribe()
+            .addDisposableTo(disposeBag)
+        
+        logined = noUser.map { !$0 }
+        
+        let user = optionalUser.filterNil().shareReplay(1)
+            
+        avatarURL = user
+            /// 只有拿到 User 才发出 avatar_url
+            .map { NSURL(string: $0.avatar_url) }
+            .filterNil()
+            .shareReplay(1)
+        
+        userName = user.asObservable().map { $0.name }.shareReplay(1)
+        
+        
+    }
+    
+    func save(type: TokenType, token: Token) {
+        KeychainService.save(.GitHub, token: token)
+        tokenSubject.onNext(token)
+    }
+    
+}
