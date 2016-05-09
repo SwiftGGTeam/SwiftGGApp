293e032e18d4935b9ec8e34a5271501a3180eec3
DianQK <xiaoqing@dianqk.org>
2016-05-09 11:15:14 +0800

    Fix crash when json error

--- GGQ/Extensions/Rx/RxMoyaX/Observable+Object.swift
+++ GGQ/Extensions/Rx/RxMoyaX/Observable+Object.swift
@@ -16,6 +16,12 @@ extension ObservableType where E == Response {
     func mapJSON() -> Observable<JSON> {
         return flatMap { response -> Observable<JSON> in
             let json = JSON(data: response.data)
+            if let error = json.error {
+                return Observable.error(error)
+            }
+            if json.type == .Null {
+                return Observable.empty()
+            }
             return Observable.just(json)
         }
     }
@@ -23,6 +29,12 @@ extension ObservableType where E == Response {
     func gg_mapJSON() -> Observable<JSON> {
         return flatMap { response -> Observable<JSON> in
             let json = JSON(data: response.data)
+            if let error = json.error {
+                return Observable.error(error)
+            }
+            if json.type == .Null {
+                return Observable.empty()
+            }
             return Observable.just(json["data"])
         }
     }
@@ -30,6 +42,12 @@ extension ObservableType where E == Response {
     func gg_storeArray<T: Object>(type: T.Type) -> Observable<Void> {
         return flatMap { response -> Observable<Void> in
             let json = JSON(data: response.data)
+            if let error = json.error {
+                return Observable.error(error)
+            }
+            if json.type == .Null {
+                return Observable.empty()
+            }
             guard let data = json["data"].arrayObject else { return Observable.empty() }
             let update = T.primaryKey() != nil
             return Realm.rx_create(type, values: data, update: update)
@@ -39,6 +57,12 @@ extension ObservableType where E == Response {
     func gg_storeObject<T: Object>(type: T.Type) -> Observable<Void> {
         return flatMap { response -> Observable<Void> in
             let json = JSON(data: response.data)
+            if let error = json.error {
+                return Observable.error(error)
+            }
+            if json.type == .Null {
+                return Observable.empty()
+            }
             let update = T.primaryKey() != nil
             return Realm.rx_create(type, value: json["data"].object, update: update)
         }
