fb02d809963e097ad80d275fbc89b8bd494f3bb8
DianQK <xiaoqing@dianqk.org>
2016-05-09 11:14:15 +0800

    添加 Cache

---
+++ GGQ/Tools/Cache.swift
@@ -0,0 +1,30 @@
+//
+//  Cache.swift
+//  GGQ
+//
+//  Created by 宋宋 on 5/8/16.
+//  Copyright © 2016 org.dianqk. All rights reserved.
+//
+
+import Foundation
+
+let articleCache: NSCache = {
+    let cache = NSCache()
+    cache.name = "gg.swift.articleCache"
+    return cache
+}()
+
+extension NSCache {
+    subscript(key: AnyObject) -> AnyObject? {
+        get {
+            return objectForKey(key)
+        }
+        set {
+            if let value: AnyObject = newValue {
+                setObject(value, forKey: key)
+            } else {
+                removeObjectForKey(key)
+            }
+        }
+    }
+}
--- GGQ/ViewControllers/ArticleViewController/ArticleManagerViewModel.swift
+++ GGQ/ViewControllers/ArticleViewController/ArticleManagerViewModel.swift
@@ -67,104 +67,99 @@ final class ArticleManagerViewModel {
                 }
             }
         }.addDisposableTo(disposeBag)
-
-        let attributedString = articleShare
-        /// 获取 NSAttributedString ，如果有 Cache ，直接拿
-        .flatMapLatest { article -> Observable<NSAttributedString> in
-//            let data: NSData
-//            if let cache = article.cacheData {
-//                data = cache
-//            } else {
-//                data = article.content.dataUsingEncoding(NSUTF8StringEncoding)!
-//                if let realm = article.realm {
-//                    do {
-//                        try realm.write {
-//                            article.cacheData = data
-//                        }
-//                    } catch {
-//                        log.error("Realm write \(articleInfo) : \(error)")
-//                    }
-//                }
-//            }
-//            
-//            return Observable.just(data)
-//                    .observeOn(.Serial(.Background))
-//                    .map { data in
-//                        let node = Node(markdown: str as String)!
-//                        let document = CMDocument(data: data, options: .Normalize)
-//                        let renderer = CMAttributedStringRenderer(document: document, attributes: GGConfig.Article.textAttributes)
-//                        return renderer.render()
-//                }
-            return Observable.just(article.content).observeOn(.Serial(.Background)).map(mdRender)
-        }.observeOn(.Main).shareReplay(1)
         
-        Observable.combineLatest(articleShare, attributedString) { (article: $0, str: $1) }
-        /// 处理总页数
-        .map { article, str in
-            if let pagerTotal = article.pagerTotal.value {
-                return pagerTotal
-            }
-            let textStorage = NSTextStorage(attributedString: str)
-
-            let textLayout = NSLayoutManager()
-            textStorage.addLayoutManager(textLayout)
-
-            let atextContainer1 = NSTextContainer(size: CGSize(width: contentSize.width, height: CGFloat.max))
-            textLayout.addTextContainer(atextContainer1)
-
-            let textViewHeight1 = textLayout.boundingRectForGlyphRange(NSRange(location: 0, length: str.length), inTextContainer: atextContainer1).size.height
-
-            textLayout.removeTextContainerAtIndex(0)
-
-            let atextContainer2 = NSTextContainer(size: contentSize)
-            textLayout.addTextContainer(atextContainer2)
-
-            let textViewHeight2 = textLayout.boundingRectForGlyphRange(NSRange(location: 0, length: str.length), inTextContainer: atextContainer2).size.height
-
-            textLayout.removeTextContainerAtIndex(0)
-
-            let count = Int(textViewHeight1 / textViewHeight2) + 1
-            
-            /// 对总页数进行缓存
-            if let realm = articleInfo.realm {
-                do {
-                    try realm.write {
-                        article.pagerTotal.value = count
-                    }
-                } catch {
-                    Error("Realm write \(articleInfo) : \(error)")
+        if let cacheAttributedStrings = articleCache[articleInfo.id] as? [NSAttributedString] {
+            elements.value = cacheAttributedStrings
+            pagerTotal.value = cacheAttributedStrings.count
+        } else {
+            let attributedString = articleShare
+                /// 获取 NSAttributedString ，如果有 Cache ，直接拿
+                .flatMapLatest { article -> Observable<NSAttributedString> in
+                    return Observable.just(article.content)
+                        .map { $0
+                            .stringByReplacingOccurrencesOfString("<center>", withString: "")
+                            .stringByReplacingOccurrencesOfString("</center>", withString: "")
+                            .stringByReplacingOccurrencesOfString("http://swift.gg/20", withString: "swiftgg://swift.gg/20")
+                        }
+                        //                    .map { $0.stringByReplacingOccurrencesOfString("> 本文由 SwiftGG 翻译组翻译，已经获得作者翻译授权，最新文章请访问 [http://swift.gg](http://swift.gg)。", withString: "") }
+                        .observeOn(.Serial(.Background))
+                        .map { mdRender(markdown: $0) }
                 }
-            }
+                .observeOn(.Main)
+                .shareReplay(1)
             
-            return count
+            Observable.combineLatest(articleShare, attributedString) { (article: $0, str: $1) }
+                /// 处理总页数
+                .map { article, str in
+                    if let pagerTotal = article.pagerTotal.value {
+                        return pagerTotal
+                    }
+                    let textStorage = NSTextStorage(attributedString: str)
+                    
+                    let textLayout = NSLayoutManager()
+                    textStorage.addLayoutManager(textLayout)
+                    
+                    let atextContainer1 = NSTextContainer(size: CGSize(width: contentSize.width, height: CGFloat.max))
+                    textLayout.addTextContainer(atextContainer1)
+                    
+                    let textViewHeight1 = textLayout.boundingRectForGlyphRange(NSRange(location: 0, length: str.length), inTextContainer: atextContainer1).size.height
+                    
+                    textLayout.removeTextContainerAtIndex(0)
+                    
+                    let atextContainer2 = NSTextContainer(size: contentSize)
+                    textLayout.addTextContainer(atextContainer2)
+                    
+                    let textViewHeight2 = textLayout.boundingRectForGlyphRange(NSRange(location: 0, length: str.length), inTextContainer: atextContainer2).size.height
+                    
+                    textLayout.removeTextContainerAtIndex(0)
+                    
+                    let count = Int(textViewHeight1 / textViewHeight2) + 1
+                    
+                    /// 对总页数进行缓存
+                    if let realm = articleInfo.realm {
+                        do {
+                            try realm.write {
+                                article.pagerTotal.value = count
+                            }
+                        } catch {
+                            Error("Realm write \(articleInfo) : \(error)")
+                        }
+                    }
+                    
+                    return count
+                }
+                .bindTo(pagerTotal)
+                .addDisposableTo(disposeBag)
+            
+            let textLayoutManager = attributedString
+                .map { str -> (textStorage: NSTextStorage, textLayout: NSLayoutManager, str: NSAttributedString) in
+                    let textStorage = NSTextStorage(attributedString: str)
+                    
+                    let textLayout = NSLayoutManager()
+                    textStorage.addLayoutManager(textLayout)
+                    Info("\(str)")
+                    return (textStorage: textStorage, textLayout: textLayout, str: str)
+                }.shareReplay(1)
+            
+            Observable.combineLatest(pagerTotal.asObservable(), textLayoutManager) { (count: $0, $1) }
+                /// 切分最新的一页
+                .map { count, text in
+                    var atts: [NSAttributedString] = []
+                    for _ in 0..<count {
+                        let atextContainer = NSTextContainer(size: contentSize)
+                        text.textLayout.addTextContainer(atextContainer)
+                        let range = text.textLayout.glyphRangeForTextContainer(atextContainer)
+                        atts.append(text.str.attributedSubstringFromRange(range))
+                    }
+                    return atts
+                }
+                .bindTo(elements)
+                .addDisposableTo(disposeBag)
+            
+            elements.asObservable().skip(1).subscribeNext {
+                articleCache[articleInfo.id] = $0
+                }.addDisposableTo(disposeBag)
         }
-            .bindTo(pagerTotal)
-            .addDisposableTo(disposeBag)
-
-        let textLayoutManager = attributedString
-            .map { str -> (textStorage: NSTextStorage, textLayout: NSLayoutManager, str: NSAttributedString) in
-                let textStorage = NSTextStorage(attributedString: str)
-
-                let textLayout = NSLayoutManager()
-                textStorage.addLayoutManager(textLayout)
-                Info("\(str)")
-                return (textStorage: textStorage, textLayout: textLayout, str: str)
-        }.shareReplay(1)
-
-        Observable.combineLatest(pagerTotal.asObservable(), textLayoutManager) { (count: $0, $1) }
-        /// 切分最新的一页
-        .map { count, text in
-            var atts: [NSAttributedString] = []
-            for _ in 0..<count {
-                let atextContainer = NSTextContainer(size: contentSize)
-                text.textLayout.addTextContainer(atextContainer)
-                let range = text.textLayout.glyphRangeForTextContainer(atextContainer)
-                atts.append(text.str.attributedSubstringFromRange(range))
-            }
-            return atts
-        }
-            .bindTo(elements)
-            .addDisposableTo(disposeBag)
 
         let json = GGProvider
             .request(GGAPI.ArticleDetail(articleId: articleInfo.id))
@@ -184,6 +179,14 @@ final class ArticleManagerViewModel {
             }
             .bindTo(updated)
             .addDisposableTo(disposeBag)
+        
+        updated.asObservable()
+            .skip(1)
+            .subscribeNext {
+                if $0 {
+                    articleCache[articleInfo.id] = nil
+                }
+            }.addDisposableTo(disposeBag)
 
     }
 
